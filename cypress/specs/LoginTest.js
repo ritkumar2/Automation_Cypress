d4c184165058663c61206873398edd4e63bbde28
ritkumar2 <ritesh.kumar2@clarivate.com>
2023-03-06 15:06:18 +0530

    Login features

--- cypress/specs/LoginTest.js
+++ cypress/specs/LoginTest.js
@@ -17,15 +17,12 @@ describe('My First Test Suite',function()
 
     // Enter user credential
         loginPage.enterUserName().type(this.data.name)
-        // cy.get('input[placeholder="Username"]').type(this.data.name)
         loginPage.enterPassword().type(this.data.password)
-        // cy.get('#user_password').type(this.data.password)
-        // cy.get('.submit').click();
-        loginPage.loginBtn().click();
+         loginPage.loginBtn().click();
+
          cy.config('pageLoadTimeout')
 
     // Navigate module page
-
          this.data.moduleName.forEach(function(element){
          cy.selectModule(element)
          })
